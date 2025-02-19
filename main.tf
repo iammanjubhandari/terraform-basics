53575b346d7af5b9a4b2deafcc30a2ba7fbb4eea
iammanjubhandari <iammanjunath.bhandari20@gmail.com>
2025-02-19 15:31:15 +0530

    Initial commit

---
+++ main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+    region = "ap-south-1"
+}
+
+resource "aws_instance" "demo-server" {
+    ami = "ami-00bb6a80f01f03502"
+    instance_type = "t2.micro"
+    key_name = "manju-aws-demo-key"
+
+    tags = {
+    Name = "demo-server"
+    evn = "dev"
+}
+}
+
+
+output "instance_public_ip" {
+    description = "Public IP of the created Ec2 instance"
+    value = aws_instance.demo-server.public_ip
+}
+
+output "instance_id" {
+    description = "Instance IP of the created Ec2 instance"
+    value = aws_instance.demo-server.id
+}
